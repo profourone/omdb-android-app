560f2e920cada8dced20e6ba6707940ba4cee81f
Rafał Ozga <profourone@gmail.com>
2022-04-10 23:24:29 +0200

    Add view binding to main activity

--- app/src/main/java/com/profourone/omdb/feature/MainActivity.kt
+++ app/src/main/java/com/profourone/omdb/feature/MainActivity.kt
@@ -2,11 +2,15 @@ package com.profourone.omdb.feature
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.profourone.omdb.R
+import com.profourone.omdb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
     }
 }
